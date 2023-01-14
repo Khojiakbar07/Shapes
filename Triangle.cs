bd19af5653e18831fb6194674ea62afa3e9374e1
Hojiakbar Zokirov <108798105+Khojiakbar07@users.noreply.github.com>
2023-01-14 13:45:53 +0500

    Update Triangle.cs

--- Triangle.cs
+++ Triangle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,62 +6,29 @@ using System.Threading.Tasks;
 
 namespace Shape
 {
-    internal class Triangle 
+    internal class Triangle : Shape, Drawable
     {
         private double a;
         private double b;
         private double c;
         private bool access;
-        
-
-        public Triangle(String color, double a, double b, double c)
+        public Triangle(string Color, string typeShape) : base(Color, typeShape)
         {
-
-            this.a = a;
-            this.b = b;
-            this.c = c;
-            if ((this.a + this.b > this.c) && (this.a + this.c > this.b) && (this.b + this.c > this.a))
-            {
-                this.access = true;
-            }
-            else
-            {
-                this.access = false;
-            }
+ 
         }
-        public void info(string color)
+        public void Draw()
         {
-            Console.WriteLine("***Shape is Triangle***");
-            Console.WriteLine("Color: " + color);
-            Console.WriteLine("a (side-1): " + this.a);
-            Console.WriteLine("b (side-2): " + this.b);
-            Console.WriteLine("c (side-3): " + this.c);
-            Console.WriteLine("---------------------");
+            Console.WriteLine("--------------------------");
         }
-        public double calculateArea(double area)
+
+        public double perimeter()
         {
             if (this.access)
             {
-                double p;
-                p = (this.a + this.b + this.c) / 2;
-                area = Math.Sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-                Console.WriteLine("Area (Triangle): " + area);
-                return area;
-            }
-            else
-            {
-                Console.WriteLine("Wrong sides for triangle to calculate area");
-                return 0.0;
-            }
-        }
-        public double calculatePerimeter(double perimeter)
-        {
-            if (this.access)
-            {
-             
+
                 double p;
                 p = this.a + this.b + this.c;
-                Console.WriteLine("Perimeter (Triangle): " + perimeter);
+                Console.WriteLine("Perimeter (Triangle): " + p);
                 return 0.0;
             }
             else
@@ -70,6 +37,23 @@ namespace Shape
                 return 0.0;
             }
         }
+        public double area()
+        {
+            if (this.access)
+            {
+
+                double Area;
+                Area = (this.a + this.b) * 2;
+                Console.WriteLine("Area (Triangle): " + Area);
+                return 0.0;
+            }
+            else
+            {
+                Console.WriteLine("Wrong sides for triangle to calculate area");
+                return 0.0;
+            }
+        }
     }
 }
+
     
