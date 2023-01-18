1dc5de7ead59dc1b4b871844983b717b5d8a1b71
Hojiakbar Zokirov <108798105+Khojiakbar07@users.noreply.github.com>
2023-01-18 13:49:50 +0500

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class Circle : Shape , Drawable
+    {
+        private double radius;
+        public Circle(string Color, string typeShape, double radius) : base(Color, typeShape)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("--------------Shape is Circle--------------");
+        }
+
+        public double perimeter()
+        {
+            double p;
+            p = 2 * Math.PI * this.radius;
+            Console.WriteLine("Perimeter (Circle): " + p);
+            return 0.0;
+        }
+        public double area()
+        {
+            double Area;
+            Area =Convert.ToInt32(Math.PI * (this.radius) * (this.radius));
+            Console.WriteLine("Area (Circle): " + Area);
+            return 0.0;
+
+        }
+    }
+}
