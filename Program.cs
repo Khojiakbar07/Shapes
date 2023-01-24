324e715ddbf72a634109ca423ebc87e0474561a0
Hojiakbar Zokirov <108798105+Khojiakbar07@users.noreply.github.com>
2023-01-24 17:31:35 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,67 +14,101 @@ namespace Shape
         {
             Rectangle rec = new Rectangle("Red", "Rectangle", 5, 5);
             Triangle tri = new Triangle("Blue", "Triangle", 3, 4, 5);
-            Circle circle = new Circle("Green", "Circle", 2);
+            Circle circle = new Circle("Green", "Circle", 3);
             Console.WriteLine("Choose one of the given shapes: ");
             Console.WriteLine("1. Rectangle");
             Console.WriteLine("2. Triangle");
             Console.WriteLine("3. Circle");
             String shapes = Console.ReadLine();
+             /*
             if (shapes == "Rectangle")
             {
                 rec.Draw();
-                rec.perimeter();
-                rec.area();
             }
             else if (shapes == "Triangle")
             {
                 tri.Draw();
-                tri.perimeter();
-                tri.area();
+
             }
             else if (shapes == "Circle")
             {
                 circle.Draw();
-                circle.perimeter();
-                circle.area();
             }
             else
             {
                 Console.WriteLine($"{shapes}: is unknown shape");
+                return 0;
             }
-
-            Console.WriteLine("Choose one of the given shapes: ");
-            Console.WriteLine("1. Rectangle Perimeter or Rectangle Area");
-            Console.WriteLine("2. Triangle Perimeter or Triangle Area");
-            Console.WriteLine("3. Circle Perimeter or Circle Area");
-
+           */
             switch (shapes)
             {
-                case "Rectangle Perimeter":
-                    Console.WriteLine($"{rec.perimeter()}");
+                case "Rectangle ":
+                    rec.Draw();
                     break;
-                case "Rectangle Area":
-                    Console.WriteLine($"{rec.area()}");
+                case "Triangle ":
+                    tri.Draw();
                     break;
-                case "Triangle Perimeter":
-                    Console.WriteLine($"{tri.perimeter()}");
-                    break;
-                case "Triangle Area":
-                    Console.WriteLine($"{tri.area()}");
-                    break;
-                case "Circle Perimeter":
-                    Console.WriteLine($"{circle.perimeter()}");
-                    break;
-                case "Circle Area":
-                    Console.WriteLine($"{rec.area()}");
-                    break;
-                default:
-                    Console.WriteLine("Can not calculate perimeter and area");
+                case "Circle ":
+                    circle.Draw();
                     break;
 
             }
+
+            Console.WriteLine("Enter Shape name and perimeter ");
+
+            shapes = Console.ReadLine();
+            switch (shapes)
+            {
+                case "Rectangle perimeter":
+                    rec.perimeter();
+                    break;
+                case "Triangle perimeter":
+                    tri.perimeter();
+                    break;
+                case "Circle perimeter":
+                    circle.perimeter();
+                    break;
+
+            }
+            Console.WriteLine("Enter Shape name and Area ");
+            shapes = Console.ReadLine();
+            switch (shapes)
+            {
+                case "Rectangle area":
+                    rec.area();
+                    break;
+                case "Triangle area":
+                    tri.area();
+                    break;
+                case "Circle area":
+                    circle.area();
+                    break;
+            }
             Console.ReadLine();
+            /*
+            shapes = Console.ReadLine();
+            switch (shapes)
+            {
+                case "Rectangle perimeter":
+                    rec.perimeter();
+                    break;
+                case "Rectangle area":
+                    rec.area();
+                    break;
+                case "Triangle perimeter":
+                    tri.perimeter();
+                    break;
+                case "Triangle area":
+                    tri.area();
+                    break;
+                case "Circle perimeter":
+                    tri.perimeter();
+                    break;
+                case "Circle area":
+                    circle.area();
+                    break;
+            }
+            */
         }
-       
     }
 }
